35d82514f73a873758162a3160eb2615ba692f20
AAArtyZ <121504064+AAArtyZ@users.noreply.github.com>
2022-12-27 21:58:20 +0700

    Create lgofspeed.lua

---
+++ lgofspeed.lua
@@ -0,0 +1,34 @@
+while true do
+wait(2.8)
+   local args = {
+        [1] = "collectOrb",
+        [2] = "Blue Orb",
+        [3] = "City"
+    }
+    
+    game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+    
+    local args = {
+        [1] = "collectOrb",
+        [2] = "Gem",
+        [3] = "City"
+    }
+    
+    game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+    
+    local args = {
+        [1] = "collectOrb",
+        [2] = "Yellow Orb",
+        [3] = "City"
+    }
+    
+    game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+    
+    local args = {
+        [1] = "collectOrb",
+        [2] = "Red Orb",
+        [3] = "City"
+    }
+    
+    game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+end
